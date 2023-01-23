cd1e0a3a5f48703415f38c2e9d39154148b585d3
adhito18 <henryalvin964@gmail.com>
2023-01-23 11:21:43 +0700

    create new all

---
+++ git-commands.js
@@ -0,0 +1,6 @@
+// 1. git init(inside project directory) (warning: first inisiate)
+// 2. git add index.js / git add .
+// 3. git commit -m "create variable inside index.js"
+// 4. git log
+// 5. git remote add origin https://github.com/adhito18/jcwd22302-scm.git (warning first inisiate)
+// 6. git push origin master
